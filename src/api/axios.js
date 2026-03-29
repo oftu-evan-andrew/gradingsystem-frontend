8c35c5c28fc14fecdc0b86265f6f090c1c0d0d6e
oftu-evan-andrew <107543784+oftu-evan-andrew@users.noreply.github.com>
2026-03-29 15:00:47 +0800

    fixed api prefix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://gradingsystem-api.onrender.com',
+    baseURL: 'https://gradingsystem-api.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
